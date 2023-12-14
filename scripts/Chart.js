80945d158fe048b0fa070c704750914445e04d98
Brenda Ross <BRoss@icstars.org>
2023-12-14 00:26:45 -0600

    commit changes
    chart.html
    script\Chart.js-scripts\chart.js
    styles\chart.css
    styles\createTool.css

--- scripts/Chart.js
+++ scripts/Chart.js
@@ -5,14 +5,30 @@ function askNewTool(action) {
     var sharpen = confirm('Would you like to sharpen the tool?');
     if (sharpen) {
       // Code to handle sharpening the tool (You can add your logic here)
-      console.log('Tool sharpened!');
+      confirmSharpened(); // Call the confirmation function
     } else {
       // Code if user chooses not to sharpen the tool
       console.log('Tool not sharpened.');
     }
   }
 }
+    
 
+// Function to confirm sharpening the tool
+function confirmSharpened() {
+  var confirmSharpen = confirm('Are you sure you want to confirm the tool as sharpened?');
+  if (confirmSharpen) {
+    // Code to confirm the tool as sharpened (You can add your logic here)
+    console.log('Tool confirmed as sharpened!');
+  } else {
+    // Code if user cancels the confirmation
+    console.log('Sharpen confirmation canceled.');
+  }
+}
+
+
+
+      //
 // Function to ask about creating a new tool
 function createNewTool(action) {
   if (action === 'Create') {
@@ -33,14 +49,26 @@ function checkToolScrap(action) {
     var scrap = confirm('Do you want to mark the tool as scrap?');
     if (scrap) {
       // Code to handle marking the tool as scrap (You can add your logic here)
-      console.log('Tool marked as scrap!');
+      confirmScrapped(); // Call the confirmation function
     } else {
+      console.log('Tool marked as scrap!');
       // Code if user chooses not to mark the tool as scrap
       console.log('Tool not marked as scrap.');
     }
   }
 }
 
+// Function to confirm marking the tool as scrap
+function confirmScrapped() {
+  var confirmScrap = confirm('Are you sure you want to confirm the tool as scrapped?');
+  if (confirmScrap) {
+    // Code to confirm the tool as scrapped (You can add your logic here)
+    console.log('Tool confirmed as scrapped!');
+  } else {
+    // Code if user cancels the confirmation
+    console.log('Scrap confirmation canceled.');
+  }
+}
   
   
  
